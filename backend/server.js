177ac6522826798604e147cc56258c6048e86162
kucse160235 <afnan1635@cseku.ac.bd>
2022-03-01 19:56:46 +0600

    MongoDB Connection and Model Creation Done

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,13 @@
-const express = require('express');
-const dotenv = require('dotenv');
-const products = require('./data/products');
+import express from 'express';
+import dotenv from 'dotenv';
+import colors from 'colors';
+import connectDB from './config/db.js';
+
+import products from './data/products.js';
 
 dotenv.config();
+
+connectDB();
 
 const app = express();
 
@@ -24,6 +29,6 @@ const PORT = process.env.PORT || 5000;
 app.listen(
   PORT,
   console.log(
-    `Server is listeing in ${process.env.NODE_ENV} mode to ${PORT} Port`
+    `Server is listeing in ${process.env.NODE_ENV} mode to ${PORT} Port`.yellow
   )
 );
